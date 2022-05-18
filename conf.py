cc693833bd0b3645770f0801d89f5810159f34a9
Oskyce <OSCARCENTRO@GMAIL.COM>
2022-05-18 10:43:22 +0200

    ok

--- conf.py
+++ conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Linee guida per l''Utilizzo delle Risorse Informatiche'
+copyright = '2022, DGSIA-CUS'
+author = 'CUS'
 
 release = '0.1'
 version = '0.1.0'
