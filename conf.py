f0648b850965c61ea38e3674a4e96f3b0184bf29
Oskyce <OSCARCENTRO@GMAIL.COM>
2022-05-18 14:42:47 +0200

    ok

--- conf.py
+++ conf.py
@@ -51,5 +51,14 @@ html_short_title = "Linee guida URI 2022"
 
 html_theme = 'sphinx_rtd_theme'
 
+# These folders are copied to the documentation's HTML output
+html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
+
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
