af1237e2d19bd7a3ddbb3437804a7709118151ff
Oskyce <OSCARCENTRO@GMAIL.COM>
2022-05-19 10:06:19 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -27,18 +27,18 @@ intersphinx_mapping = {
 }
 intersphinx_disabled_domains = ['std']
 
-templates_path = ['_templates']
+#templates_path = ['_templates']
 
 # These folders are copied to the documentation's HTML output
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
 
-html_context = {
-    'css_files': [
-    '_static/css/theme.css',
-    ],
+#html_context = {
+#    'css_files': [
+#    '_static/css/theme.css',
+#    ],
 #        'reference_project': 'Linee guida per utilizzo risorse informatiche',
 #       'reference_project_url': 'https://                /',
     }
